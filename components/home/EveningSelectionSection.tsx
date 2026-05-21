3031133c5589090df6cca2be73305afed11be1c0
Xusratbek <matsapayevxusrat@gmail.com>
2026-05-21 14:06:44 +0500

    ffsfds

--- components/home/EveningSelectionSection.tsx
+++ components/home/EveningSelectionSection.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
-import HomeSection from '../ui/HomeSection';
-import EveningSelectionCard from '../EveningSelectionCard';
+// import React from 'react';
+// import EveningSelectionCard from '../EveningSelectionCard';
+// import HomeSection from '../ui/HomeSection';
 
-const EVENING_POSTER = require('../../assets/images/final_destination_poster.png');
+// const EVENING_POSTER = require('../../assets/images/poster.jpg');
 
-const noop = () => {};
+// const noop = () => {};
 
-export default function EveningSelectionSection() {
-  return (
-    <HomeSection
-      title="Идеально под ваш вечер"
-      subtitle="Алгоритм подобрал по времени и настроению"
-    >
-      <EveningSelectionCard
-        title="Пункт Назначения"
-        subtitle="Ужасы / Триллер"
-        ratingKinopoisk={7.9}
-        ratingImdb={8.3}
-        duration="2ч 15м"
-        poster={EVENING_POSTER}
-        onPress={noop}
-      />
-    </HomeSection>
-  );
-}
+// export default function EveningSelectionSection() {
+//   return (
+//     <HomeSection
+//       title="Идеально под ваш вечер"
+//       subtitle="Алгоритм подобрал по времени и настроению"
+//     >
+//       <EveningSelectionCard
+//         title="Пункт Назначения"
+//         subtitle="Ужасы / Триллер"
+//         ratingKinopoisk={7.9}
+//         ratingImdb={8.3}
+//         duration="2ч 15м"
+//         poster={EVENING_POSTER}
+//         onPress={noop}
+//       />
+//     </HomeSection>
+//   );
+// }
